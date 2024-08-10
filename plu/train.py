c8318d8262528b5f07870a9f9aed0b97a4ad006c
Volodymyr Kyrylov <vol@wilab.org.ua>
2024-08-10 19:33:43 +0300

    logging tweaks

--- plu/train.py
+++ plu/train.py
@@ -375,7 +375,7 @@ def main():
 
         if global_step % args.logging_steps == 0:
             accelerator.log({"train/running_loss": running_loss / args.logging_steps}, step=global_step)
-            labels = batch['labels']
+            labels = batch['labels'][:,0]
             tokens = outputs.logits[:,0].argmax(-1)
             probs = outputs.logits[:,0].softmax(dim=-1)
             probs_ = probs.max(-1).values.detach().tolist()
@@ -383,7 +383,7 @@ def main():
             labels = labels.tolist()
             tokens = tokens.tolist()
             no_speech = 50363
-            labelprobs = probs[:,no_speech]
+            labelprobs = probs[:,no_speech].detach().tolist()
             logger.info(f"running loss: {running_loss / args.logging_steps} vad accuracy: {acc} first tokens: {tokens} probs: {probs_} labels: {labels} labelprobs: {labelprobs}")
             running_loss = 0
 
